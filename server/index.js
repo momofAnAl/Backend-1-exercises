d9ce87bff665ad5cd2add4ee28f5e87a3bff241b
Anh Tran <anhtr077@gmail.com>
2024-01-16 18:20:09 -0800

    Adding files for first back-end work with Javascript

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const cors = require("cors");
+
+const app = express();
+app.use(express.json()); 
+
+app.use(cors());
+app.get("/api/users", (req, res) => {
+    let friends = ["Albert","Anna", "Camelia", "Riley"];
+    res.status(200).send(friends);
+});
+app.get("/weather/:tempature", (req, res) => {
+    const {tempature} = req.params;
+    const phrase = `<h3> It was ${tempature} today</h3>` ;
+    res.status(200).send(phrase);
+});
+
+app.listen(4000, () => console.log("Server running on port 4000"));
